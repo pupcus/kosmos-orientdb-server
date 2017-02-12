7869bb943142b44f8340e4d73b32c0b11aa85975
Michael Pendergrass <mdpendergrass@gmail.com>
2017-02-12 09:31:00 -0500

    update dependencies

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject kosmos/kosmos-orientdb-server "0.0.1-SNAPSHOT"
+(defproject kosmos/kosmos-orientdb-server "0.0.2-SNAPSHOT"
 
   :description "orientdb server component"
 
@@ -9,17 +9,17 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :dependencies [[kosmos "0.0.5"]
+  :dependencies [[kosmos "0.0.6"]
                  [org.clojure/tools.logging "0.3.1"]
 
-                 [com.orientechnologies/orientdb-core "2.2.14"]
-                 [com.orientechnologies/orientdb-server "2.2.14"]
-                 [com.orientechnologies/orientdb-distributed "2.2.14"]]
+                 [com.orientechnologies/orientdb-core "2.2.16"]
+                 [com.orientechnologies/orientdb-server "2.2.16"]
+                 [com.orientechnologies/orientdb-distributed "2.2.16"]]
 
   :profiles {:dev {:resource-paths ["dev-resources"]
                    :dependencies   [[org.clojure/clojure "1.8.0"]
                                     [hiccup "1.0.5"]
-                                    [org.clojure/java.jdbc "0.4.2"]
+                                    [org.clojure/java.jdbc "0.6.1"]
                                     [org.slf4j/slf4j-log4j12 "1.7.5"]]}}
 
   :deploy-repositories [["snapshots"
